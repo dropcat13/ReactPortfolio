e6fd8a9116e809a18da4dbb9bbc8205802e623da
Cat Kretzschmar <cat.ktz@gmail.com>
2020-04-03 14:47:08 -0400

    Updates

---
+++ precache-manifest.2dcc373271ea509fbb70a50dc7f581f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b1031e24f3072a7058e71e9967b0d19",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "9388178a7dbb1413781d",
+    "url": "/ReactPortfolio/static/css/main.56524683.chunk.css"
+  },
+  {
+    "revision": "6016a1bd0005f7989006",
+    "url": "/ReactPortfolio/static/js/2.0652f8bf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactPortfolio/static/js/2.0652f8bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9388178a7dbb1413781d",
+    "url": "/ReactPortfolio/static/js/main.a6f81bba.chunk.js"
+  },
+  {
+    "revision": "6ebc7d7ffe507d72e622",
+    "url": "/ReactPortfolio/static/js/runtime-main.5489e340.js"
+  }
+]);
